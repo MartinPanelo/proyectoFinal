40289ee87e850719fc578193cf6888cb8c3f3d1b
MartinPanelo <panelomartinn@hotmail.com>
2022-10-18 23:54:44 -0300

    favoritos(100%)

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 init(); // establecer estado incial
 function init(){
     document.getElementById("c").style.visibility = "hidden";
+    document.getElementById("l").style.visibility = "hidden";
 };
 
 
@@ -9,5 +10,13 @@ function verFiltro(){
         document.getElementById("c").style.visibility = "hidden";
     }else{
         document.getElementById("c").style.visibility = "visible"
+    };
+};
+
+function verFavoritos(){
+    if(document.getElementById("l").style.visibility == "visible"){
+        document.getElementById("l").style.visibility = "hidden";
+    }else{
+        document.getElementById("l").style.visibility = "visible"
     };
 };
