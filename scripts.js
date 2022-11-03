adb184f75d8c91aefbab5a6051a1331d4178d70e
MartinPanelo <panelomartinn@hotmail.com>
2022-11-02 22:35:50 -0300

    primer version banner /  pagina 2/ pagina 3

--- scripts.js
+++ scripts.js
@@ -3,14 +3,52 @@ btnT  = document.getElementById("btnT");
 catalogo = document.getElementById("categ");
 lista = document.getElementById("lista");
 logo2 = document.getElementById("logo-2");
+imgC =  document.getElementById("idCarrusel");
+let imgCarrusel = ["img1","img2","img3"];
+let i = 0;
 init(); // establecer estado incial
 function init(){
     catalogo.style.visibility = "hidden";
     lista.style.visibility = "hidden";
     logo2.style.display = "none";
     btnT.style.display = "none";
+    imgC.src = "./../assets/carrusel/"+ imgCarrusel[i] + ".jpg";
+    myTimer = setInterval(cambiarimg, 5000);
+ /*    cambiarimg(); */
+   
+/* setInterval(cambiarimg, 5000); */
 };
 
+// CARRUSEL
+
+function pasarfoto(direccion){// tengo que resetear el contador 
+
+ clearInterval(myTimer);
+ myTimer = setInterval(cambiarimg, 5000);
+cambiarimg(direccion);
+
+}
+
+
+
+
+
+/* setInterval(cambiarimg(1), 1000); */
+function cambiarimg(direccion = 1){   
+    i += direccion;
+    console.log(i);
+    if(i < imgCarrusel.length){
+        imgC.src = "./../assets/carrusel/"+ imgCarrusel[i] + ".jpg";
+    }else{
+        i = 0;
+        imgC.src = "./../assets/carrusel/"+ imgCarrusel[i] + ".jpg";
+    }
+    if(i == -1){
+        i = (imgCarrusel.length-1);
+        imgC.src = "./../assets/carrusel/"+ imgCarrusel[i] + ".jpg";
+    }
+   
+}
 
 function verFiltro(){
     if(catalogo.style.visibility == "visible"){
@@ -19,7 +57,6 @@ function verFiltro(){
         catalogo.style.visibility = "visible"
     };
 };
-
 function verFavoritos(){
     if(lista.style.visibility == "visible"){
         lista.style.visibility = "hidden";
@@ -27,21 +64,13 @@ function verFavoritos(){
         lista.style.visibility = "visible"
     };
 };
-
-
-
 function iratop ()  {
     document.body.scrollIntoView({
         behavior: "smooth",
       });
-  };
-
-
-
-
-
+};
 addEventListener("scroll",function(){
-   
+    
     console.log(barra.offsetTop);
     if(barra.offsetTop > 500){
         
