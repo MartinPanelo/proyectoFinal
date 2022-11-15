d9f7bbac1b969c7769397f93dd67193e2a07564e
MartinPanelo <panelomartinn@hotmail.com>
2022-11-15 10:53:30 -0300

    sacando archivos innecesario

--- pagina2/comprascripts.js
+++ pagina2/comprascripts.js
@@ -1,11 +1,12 @@
 parrafodescripcion = document.getElementById("des");
 modeloparte = document.getElementById("modeloparte");
+formCompra = document.forms["formcompra"];
 let caracteristicas = {
-    motor : "descripcion de motor",
+    motor : "Su avanzado motor de hidrogeno le permite tener la misma autonomía que un coche convencional sin necesidad de enchufarse y con cero emisiones contaminantes",
     motormodelo : "/assets/pagina2/motor/scene.gltf",
-    asiento : "descripcion del asiento",
+    asiento : "Tienen un respaldo confortable que se adapta a la curvatura de la espalda formando una suave y sutil ese, la cual imita la forma de la columna vertebral humana.",
     asientormodelo : "/assets/pagina2/mimi/asiento.glb",
-    rueda : "descripcion de rueda",
+    rueda : "Neumático para todas las estaciones del año que posee un innovador compuesto en la banda de rodamiento para un excelente agarre en cualquier tipo de suelo y un rodar silencioso.",
     ruedamodelo : "/assets/pagina2/mimi/rueda.glb"
 }
 
@@ -22,6 +23,7 @@ function init() {
 
 function seteardescripcion(parte) {
 
+
     if(parte == "motor"){
         parrafodescripcion.innerText = caracteristicas.motor;
         modeloparte.src = caracteristicas.motormodelo;
@@ -37,4 +39,21 @@ function seteardescripcion(parte) {
   
   
 
+}
+
+function Compra(){
+
+   console.log(formCompra["nombre"].value);
+   console.log(formCompra["apellido"].value);
+
+   console.log(formCompra["telefono"].value);
+   console.log(formCompra["pais"].value);
+
+
+
+
+
+
+
+    return false;
 }
